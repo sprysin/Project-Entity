af0fb5fdf0922723f77de869e5d3e54e7e28a0e4
DM <102883060+Sprysin@users.noreply.github.com>
2026-02-19 10:58:17 -0800

    BAD build: Generate initial debug build artifacts for ProjectEntity.Server.

--- ProjectEntity.Server/obj/Debug/net8.0/ProjectEntity.Server.AssemblyInfo.cs
+++ ProjectEntity.Server/obj/Debug/net8.0/ProjectEntity.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectEntity.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0f122fe56315fdc076d6616a543bd0c980289d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5414887ff8287ef48381dcb7134e42ee14b108")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectEntity.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectEntity.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
